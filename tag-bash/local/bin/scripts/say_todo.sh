0fd0e2dc5efbd87904cda645842c9bc59dd8d0d3
Yi-Qing <yiqing.wang.sc@foxmail.com>
2023-10-08 18:05:42 +0800

    bash

--- tag-bash/local/bin/scripts/say_todo.sh
+++ tag-bash/local/bin/scripts/say_todo.sh
@@ -6,7 +6,9 @@ do_say_todo(){
 	TODO_LINE=$(cat < "${TODO_FILE}" | wc -l)
 
 	if [ "${TODO_LINE}" -ne 0 ]; then
+        printf "\e[3;38;2;62;237;231m"
 		cat < "${TODO_FILE}" | cowthink -f koala -n # | lolcat
+        printf "\e[0m\n"
 	fi
 }
 
