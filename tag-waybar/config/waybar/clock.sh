1439b1fcdb3dc9c9f3fa6f2646eedcec8c3c6bea
Yi-Qing <yiqing.wang.sc@foxmail.com>
2023-10-17 19:07:41 +0800

    waybar

--- tag-waybar/config/waybar/clock.sh
+++ tag-waybar/config/waybar/clock.sh
@@ -7,6 +7,7 @@ IFS=" " read -r -a time_array <<< "${stringarray[0]}"
 
 TEXT="${time_array[2]}"
 ALT="${time_array[1]}"
-TOOLTIP="${stringarray[1]//生肖/}\\n${stringarray[2]}"
+TOOLTIP="${stringarray[1]//生肖/}"
+TOOLTIP="${TOOLTIP//阴历/${time_array[0]}}\\n${stringarray[2]}"
 
 printf '{"text": "%s", "alt": "%s", "tooltip": "%s"}\n' "$TEXT" "$ALT" "$TOOLTIP"
