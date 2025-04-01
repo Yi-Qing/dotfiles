62adcacded4985d7f4a286f591107f86b768dbf4
Yi-Qing <51733478+Yi-Qing@users.noreply.github.com>
2025-04-01 17:22:25 +0800

    Update wezterm.lua

--- tag-wezterm/config/wezterm/wezterm.lua
+++ tag-wezterm/config/wezterm/wezterm.lua
@@ -1,44 +1,24 @@
 local wezterm = require 'wezterm'
 
-local config = {}
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
+local config = wezterm.config_builder()
 -- config.automatically_reload_config = false
-config.check_for_updates = false
 
-config.front_end = "WebGpu"
-config.webgpu_power_preference = "LowPower" -- iGPU
-config.audible_bell= "Disabled"
+-- theme
+config.color_scheme = 'synthwave' -- or 'Isotope (base16)'
 config.bold_brightens_ansi_colors = "BrightOnly"    -- or "No"
 
-config.color_scheme = 'synthwave'   -- 'Isotope (base16)'
--- config.color_scheme = 'Isotope (base16)'   -- 'synthwave'
+-- font
+config.font = wezterm.font_with_fallback {
+    { family = 'JetBrains Mono', weight = 'Light' },
+    'Noto Color Emoji',
+}
+config.font_size = 11.0
+
+-- window
 config.initial_rows = 30
 config.initial_cols = 120
-
-config.default_cursor_style = "BlinkingBlock"
-config.animation_fps = 1
-config.cursor_blink_ease_in = 'Constant'
-config.cursor_blink_ease_out = 'Constant'
-
-config.exit_behavior = "Close"
-
-config.font = wezterm.font 'FiraCode Nerd Font Mono Light'
-config.font_size = 13.0
-
-config.scrollback_lines = 5000
-
+config.window_background_opacity = 0.7
 config.window_decorations = "NONE"
-config.window_background_opacity = 0.75
--- config.enable_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.show_new_tab_button_in_tab_bar = false
-
-config.switch_to_last_active_tab_when_closing_tab = true
-config.window_close_confirmation = 'NeverPrompt'
-
 config.window_padding = {
     top = 10,
     left = 5,
@@ -46,17 +26,54 @@ config.window_padding = {
     bottom = 0,
 }
 
+-- exit
+config.window_close_confirmation = "NeverPrompt"
+config.exit_behavior = "Close"
+
+-- Gpu rendering
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "LowPower" -- iGPU
+
+-- Tab bar
+config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_tab_index_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.switch_to_last_active_tab_when_closing_tab = true
+
+-- cursor
+config.animation_fps = 1
+config.cursor_blink_rate = 800
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.default_cursor_style = 'BlinkingBlock'
+config.hide_mouse_cursor_when_typing = true
+
+-- bell
+config.audible_bell= "Disabled"
+config.visual_bell = {
+    fade_in_duration_ms = 75,
+    fade_out_duration_ms = 75,
+    target = 'CursorColor',
+}
+
+-- misc config
+config.scrollback_lines = 5000
+config.check_for_updates = false
+config.prefer_to_spawn_tabs = true  -- prefer to spawn new tabs instead of new windows
+config.detect_password_input = true -- when user is typing a password then hide the input
+config.notification_handling = "SuppressFromFocusedPane" -- notificetaion unless focused pane
+
+-- TODO: use `wezterm show-keys --lua` to see the configed keymap
+-- local keymap = require 'keymap'
+-- keymap.apply(config)
+
+
+-- target cusomization
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    -- config.canonicalize_pasted_newlines = "CarriageReturn"
-    config.hide_tab_bar_if_only_one_tab = false
-    config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-    config.win32_system_backdrop = "Disable"
-    config.default_prog = { 'C:\\Root\\MSYS2\\usr\\bin\\bash.exe', "--login", "-i"}
-    config.set_environment_variables = {
-        MSYSTEM = "UCRT64",
-        MSYS = "winsymlinks:nativestrict",
-        CHERE_INVOKING = "enabled_from_arguments",
-    }
+    local windows = require 'windows'
+    windows.apply(config)
 end
 
 return config
